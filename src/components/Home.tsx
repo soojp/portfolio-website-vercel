17ecb9605848e8ef2731a7575904e6b37fbc544a
Soo Jee Park <talktosoojee@gmail.com>
2022-09-06 15:12:27 -0700

    made links more descriptive

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,12 +6,13 @@ const Home = () => {
       {/* page title */}
       <h1 className="text-xl text-center text-fuchsia-800 mb-4">About Me</h1>
       {/* body */}
+
       <div className="grid grid-cols-1 p-3 justify-items-center md:grid-cols-2">
         <div className=" grid justify-items-center m-6">
           <img
             src="https://soojpimages.s3.us-west-1.amazonaws.com/soojee.jpg"
             alt="Soo Jee"
-            className="w-40 h-50 rounded-full shadow-xl p-2 mb-6 items-center outline outline-offset-2 outline-rose-700 w-80 h-90 rounded-full"
+            className="w-40 h-50 shadow-xl p-2 mb-6 items-center outline outline-offset-2 outline-rose-700 w-80 h-90 rounded-full"
           />
         </div>
 
@@ -19,13 +20,13 @@ const Home = () => {
           <p className="bg-white p-3 border border-black">
             Hello, my name is Soo Jee and I am a full stack engineer who loves
             to play games (mainly Valorant) and chill with my cats (Check out
-            their Youtube Channel{" "}
+            their{" "}
             <span>
               <a
                 href="https://www.youtube.com/channel/UC4s766tDAg5mBztuzUSAAgQ"
                 className="underline text-purple-700 hover:text-fuchsia-600"
               >
-                here
+                Youtube Channel
               </a>
             </span>
             ).
@@ -38,13 +39,12 @@ const Home = () => {
             non-traditional background; I have a Bachelors in Biology and worked
             as a Clinical Laboratory Assistant before joining a coding bootcamp.
             You can see my publication as third author in{" "}
-            <span className="italic">Current Biology</span>{" "}
             <span>
               <a
                 href="https://www.sciencedirect.com/science/article/pii/S0960982222011228"
                 className="underline text-purple-700 hover:text-fuchsia-600"
               >
-                here
+                <span className="italic">Current Biology</span>
               </a>
             </span>
             .
@@ -56,7 +56,8 @@ const Home = () => {
             I have recently graduated from Coding Dojo, a coding bootcamp that
             trains people on full stack development to help them excel as
             professional software developers, and am now looking for a job. This
-            site is made using React, Typescript, and Tailwind CSS and deployed using Vercel.
+            site is made using React, Typescript, and Tailwind CSS and deployed
+            using Vercel.
           </p>
 
           <br />
